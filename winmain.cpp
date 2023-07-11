d03f3fafec638b83580daf2c3627ae8883024550
タケモン <t.ryuto.kokusairiko@gmail.com>
2023-07-11 13:50:42 +0900

    たぶんクイズ原型完成かも？

--- winmain.cpp
+++ winmain.cpp
@@ -13,6 +13,7 @@ enum
 {
 	PageTITLE,
 	PageEXPLAIN,
+	PageSYSTEM,//countstopを0にする場所
 	PageQ,//問題
 	PageA,//正解or不正解
 	PageTIPS,//問題の解説（次のページでPageQに戻る）
@@ -107,6 +108,7 @@ int select_time = 10;
 int draw_time = 0;
 
 int score = 0;
+int before_score = 0;
 int combo = 0;
 
 //正解しているか
@@ -121,22 +123,23 @@ void init()
 		if (Question[i].num == 0)
 		{
 			//問題文
-			Question[i].sentence[0] = { "次のうち、千葉県での生産量が" };
-			Question[i].sentence[1] = { "日本一である野菜・農作物はどれ？" };
+			Question[i].sentence[0] = "次のうち、千葉県での生産量が";
+			Question[i].sentence[1] =  "日本一である野菜・農作物はどれ？";
 
 			Question[i].Image_Graphic = LoadGraph("");//問題のイメージ画像を変数に読み込む
 
 			//選択肢
-			Question[i].choices[0] = { "A：Apple社" };
-			Question[i].choices[1] = { "B：落花生" };
-			Question[i].choices[2] = { "C：メロン" };
-			Question[i].choices[3] = { "D：すいか" };
+			Question[i].choices[0] = "A：Apple社";
+			Question[i].choices[1] = "B：落花生";
+			Question[i].choices[2] = "C：メロン";
+			Question[i].choices[3] = "D：すいか";
 
 			//正解（A、B、C、Dのどれかを入力）
-			Question[i].answer = { 'B'};
+			Question[i].answer = 'B';
 
 			//問題の解説
-			Question[i].tips[0] = { "国内産の落花生の8割は、千葉県で生産されています。" };
+			Question[i].tips[0] = "国内産の落花生の8割は、";
+			Question[i].tips[1] = "千葉県で生産されています。";
 		}
 	}
 }
@@ -157,14 +160,15 @@ void draw()
 	if (countstop == 0)
 	{
 		draw_time = 0;
-		if (Page == PageQ)
-		{
-			count_question++;
-		}
 		if (Page == PageA && correct == true)
 		{
-			score += 15 + combo * 5 + select_time * 2;
+			before_score = 15 + combo * 5 + select_time * 2;
 			combo++;
+			count_question++;
+		}
+		if (Page == PageTIPS && correct == true)
+		{
+			score += before_score;
 		}
 		countstop = 1;
 	}
@@ -173,58 +177,57 @@ void draw()
 		draw_time++;
 	}
 
-	DrawFormatString(450, 10, TextColor, "スコア %d 点", score);
-	DrawFormatString(450, 10, TextColor, "制限時間 %d 秒", select_time);
-
 	//ゲーム説明
 	if (Page == PageEXPLAIN)
 	{
 		if (draw_time >= 0)
 		{
-			DrawFormatString(300, 10, TextColor, "千葉県クイズ！");
+			DrawFormatString(250, 10, TextColor, "千葉県クイズ！");
 			//DrawStringToHandle(300, 10, "千葉県クイズ！", count_question, TextColor, FontHandle[1]);
 		}
 		if (draw_time >= 3)
 		{
-			DrawFormatString(300, 70, TextColor, "これから、千葉県に関するクイズを");
-			DrawFormatString(300, 105, TextColor, "10問出題します！");
+			DrawFormatString(250, 70, TextColor, "これから、千葉県に関するクイズを");
+			DrawFormatString(250, 105, TextColor, "10問出題します！");
 			//DrawStringToHandle(300, 70, "これから、千葉県に関するクイズを", TextColor, FontHandle[0]);
 			//DrawStringToHandle(300, 105, "10問出題します！", TextColor, FontHandle[0]);
 		}
 		if (draw_time >= 6)
 		{
-			DrawFormatString(300, 140, TextColor, "連続で正解したり、素早く答えて正解すると");
-			DrawFormatString(300, 175, TextColor, "スコアが大きく増えます！");
+			DrawFormatString(250, 140, TextColor, "連続で正解したり、素早く答えて正解すると");
+			DrawFormatString(250, 175, TextColor, "スコアが大きく増えます！");
 			//DrawStringToHandle(300, 140, "連続で正解したり、素早く答えて正解すると", TextColor, FontHandle[0]);
 			//DrawStringToHandle(300, 175, "スコアが大きく増えます！", TextColor, FontHandle[0]);
 		}
 		if (draw_time >= 9)
 		{
-			DrawFormatString(300, 210, TextColor, "なお、制限時間内に解答しなかった場合");
-			DrawFormatString(300, 245, TextColor, "最後まで選択していたものが解答になります");
+			DrawFormatString(250, 210, TextColor, "なお、制限時間内に解答しなかった場合");
+			DrawFormatString(250, 245, TextColor, "最後まで選択していたものが解答になります");
 			//DrawStringToHandle(300, 210, "なお、制限時間内に解答しなかった場合", TextColor, FontHandle[0]);
 			//DrawStringToHandle(300, 245, "最後まで選択していたものが解答になります", TextColor, FontHandle[0]);
 		}
 		if (draw_time >= 12)
 		{
-			DrawFormatString(300, 245, TextColor, "めざせ！ハイスコア！");
+			DrawFormatString(250, 280, TextColor, "めざせ！ハイスコア！");
 			//DrawStringToHandle(300, 280, "めざせ！ハイスコア！", TextColor, FontHandle[0]);
 		}
 	}
 	//問題文を描画する
 	if (Page == PageQ)
 	{
+		DrawFormatString(0, 10, TextColor, "スコア %d 点", score);
+		DrawFormatString(0, 45, TextColor, "制限時間 %d 秒", select_time);
 		if (draw_time >= 0)
 		{
-			DrawFormatString(300, 10, TextColor, "第 %d 問", count_question);
+			DrawFormatString(300, 10, TextColor, "第 %d 問", count_question + 1);
 		}
 		if (draw_time >= 3)
 		{
-			DrawFormatString(300, 45, TextColor, "%s", Question[i].sentence[0]);
+			DrawFormatString(300, 45, TextColor, "%s", Question[count_question].sentence[0].c_str());
 		}
 		if (draw_time >= 6)
 		{
-			DrawFormatString(300, 80, TextColor, "%s", Question[i].sentence[0]);
+			DrawFormatString(300, 80, TextColor, "%s", Question[count_question].sentence[1].c_str());
 		}
 		//選択肢表示
 		if (draw_time >= 9 && select_time > 0)
@@ -251,13 +254,13 @@ void draw()
 				DrawFormatString(310, 480, TextColor, "⇒");
 			}
 			//Aの選択肢
-			DrawFormatString(110, 260, TextColor, "%s", Question[i].choices[0]);
+			DrawFormatString(110, 260, TextColor, "%s", Question[count_question].choices[0].c_str());
 			//Bの選択肢
-			DrawFormatString(360, 260, TextColor, "%s", Question[i].choices[1]);
+			DrawFormatString(360, 260, TextColor, "%s", Question[count_question].choices[1].c_str());
 			//Cの選択肢
-			DrawFormatString(110, 480, TextColor, "%s", Question[i].choices[2]);
+			DrawFormatString(110, 480, TextColor, "%s", Question[count_question].choices[2].c_str());
 			//Dの選択肢
-			DrawFormatString(360, 480, TextColor, "%s", Question[i].choices[3]);
+			DrawFormatString(360, 480, TextColor, "%s", Question[count_question].choices[3].c_str());
 		}
 		if (select_time <= 0)
 		{
@@ -268,11 +271,18 @@ void draw()
 	//正解か、不正解かを表示
 	if (Page == PageA)
 	{
-		if (Select == Question[i].answer)
+		DrawFormatString(450, 10, TextColor, "スコア %d 点", score);
+		DrawFormatString(450, 45, TextColor, "制限時間 %d 秒", select_time);
+		if (combo >= 2)
+		{
+			DrawFormatString(450, 80, TextColor, "%d 問連続正解！", combo);
+		}
+		if (Select == Question[count_question].answer)
 		{
 			correct = true;
 			//DrawStringToHandle(300, 10, "正解！", TextColor, FontHandle[1]);
 			DrawFormatString(300, 10, TextColor, "正解！");
+			DrawFormatString(300, 45, TextColor, "スコア %d 点獲得！", before_score);
 		}
 		else {
 			correct = false;
@@ -284,17 +294,31 @@ void draw()
 	//解説を表示
 	if (Page == PageTIPS)
 	{
+		DrawFormatString(450, 10, TextColor, "スコア %d 点", score);
 		//DrawStringToHandle(300, 10, "正解は", TextColor, FontHandle[0]);
 		DrawFormatString(300, 10, TextColor, "正解は");
-		if (Question[i].num == 0)
+		if (Question[count_question].num == 0)
 		{
 			//DrawStringToHandle(300, 45, "B：落花生", TextColor, FontHandle[1]);
 			DrawFormatString(300, 45, TextColor, "B：落花生");
 		}
-		DrawFormatString(300, 70, TextColor, "%s", Question[i].tips[0]);
+		DrawFormatString(300, 70, TextColor, "%s", Question[count_question].tips[0].c_str());
+		DrawFormatString(300, 105, TextColor, "%s", Question[count_question].tips[1].c_str());
 	}
 	//問題のイメージ画像を描画
-	DrawGraph(0, 0, Question[i].Image_Graphic, true);
+	DrawGraph(0, 0, Question[count_question].Image_Graphic, true);
+	//結果発表～！
+	if (Page == PageRESULT)
+	{
+		if (draw_time >= 0)
+		{
+			DrawFormatString(300, 10, TextColor, "成績発表！");
+		}
+		if (draw_time >= 2)
+		{
+			DrawFormatString(300, 10, TextColor, "あなたのスコアは... %d 点！", score);
+		}
+	}
 }
 
 void timer()
@@ -308,7 +332,7 @@ void Start()
 	srand(time(0));
 
 	init();
-	shuffle();
+	//shuffle();
 
 	//フォント0、25ピクセル
 	//FontHandle[0] = CreateFontToHandle(NULL, 25, 3);
@@ -327,8 +351,8 @@ void Start()
 	// 描画する文字列のサイズを設定
 	SetFontSize(25);
 
-	NameColor = GetColor(255, 255, 255);//名前は白で書く
-	TextColor = GetColor(0, 0, 0);//文字は黒で書く
+	//NameColor = GetColor(255, 255, 255);//名前は白で書く
+	TextColor = GetColor(255, 255, 255);//文字は白で書く
 
 	//BGM再生
 	PlayMusic("bgm_maoudamashii_neorock71b.mp3", DX_PLAYTYPE_LOOP);
@@ -338,7 +362,9 @@ void Start()
 void Update()
 {
 	timer();
-	DrawFormatString(450, 100, TextColor, "カウント %d", update_count);
+
+	//デバッグ用
+	DrawFormatString(550, 100, TextColor, "何問目？ %d", count_question);
 
 	//キーを押してページをめくる処理
 	//エンターキー(KEY_INPUT_RETURN)が押されたらページを進める
@@ -446,7 +472,7 @@ void Update()
 	if (Page == PageTITLE)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Title_Graphic, true);
+		//DrawGraph(0, 0, Title_Graphic, true);
 
 		if (BlinkCounter / 20 % 2 == 0) {
 			DrawFormatString(300, 300, TextColor, "PUSH ENTER");
@@ -459,19 +485,23 @@ void Update()
 	if (Page == PageEXPLAIN)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Back_Graphic, true);
+		//DrawGraph(0, 0, Back_Graphic, true);
 
 		//画面にメッセ―ジウィンドウを描画します
 		//DrawGraph(0, 360, Message_Graphic, true);
 		//メッセージウィンドウにテキストを表示する
 		draw();
 	}
-
+	if (Page == PageSYSTEM)
+	{
+		countstop = 0;
+		Page++;
+	}
 	//4ページ目の場合の処理
 	if (Page == PageQ)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Back_Graphic, true);
+		//DrawGraph(0, 0, Back_Graphic, true);
 		//画面にメッセ―ジウィンドウを描画します
 		//DrawGraph(0, 360, Message_Graphic, true);
 		//メッセージウィンドウにテキストを表示する
@@ -482,7 +512,7 @@ void Update()
 	if (Page == PageA)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Back_Graphic, true);
+		//DrawGraph(0, 0, Back_Graphic, true);
 		//画面にメッセ―ジウィンドウを描画します
 		//DrawGraph(0, 360, Message_Graphic, true);
 		//メッセージウィンドウにテキストを表示する
@@ -492,7 +522,7 @@ void Update()
 	if (Page == PageTIPS)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Back_Graphic, true);
+		//DrawGraph(0, 0, Back_Graphic, true);
 		//画面にメッセ―ジウィンドウを描画します
 		//DrawGraph(0, 360, Message_Graphic, true);
 		//メッセージウィンドウにテキストを表示する
@@ -516,7 +546,7 @@ void Update()
 	if (Page == PageRESULT)
 	{
 		// 画面に背景を描画します。
-		DrawGraph(0, 0, Back_Graphic, true);
+		//DrawGraph(0, 0, Back_Graphic, true);
 
 		//画面にメッセ―ジウィンドウを描画します
 		//DrawGraph(0, 360, Message_Graphic, true);
