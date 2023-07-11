1e25523da254ee53377c2c7151f9111785c30782
タケモン <t.ryuto.kokusairiko@gmail.com>
2023-07-11 15:17:57 +0900

    takekawa.cppを更新

--- takekawa.cpp
+++ takekawa.cpp
@@ -1,5 +1,5 @@
-//1問目
-if (Question[i].num == 0)
+//6問目
+if (Question[i].num == 5)
 {
 	//問題文
 	Question[i].sentence[0] = "次のうち、千葉県での生産量が";
@@ -8,20 +8,42 @@ if (Question[i].num == 0)
 	Question[i].Image_Graphic = LoadGraph("");//問題のイメージ画像を変数に読み込む
 
 	//選択肢
-	Question[i].choices[0] = "A：Apple社";
-	Question[i].choices[1] = "B：落花生";
+	Question[i].choices[0] = "A：いちじく";
+	Question[i].choices[1] = "B：ぶり類";
 	Question[i].choices[2] = "C：メロン";
-	Question[i].choices[3] = "D：すいか";
+	Question[i].choices[3] = "D：大根";
 
 	//正解（A、B、C、Dのどれかを入力）
-	Question[i].answer = 'B';
+	Question[i].answer = 'D';
 
 	//問題の解説
 	Question[i].tips[0] = "国内産の落花生の8割は、";
 	Question[i].tips[1] = "千葉県で生産されています。";
 }
-//2問目
-if (Question[i].num == 1)
+//7問目
+if (Question[i].num == 6)
+{
+	//問題文
+	Question[i].sentence[0] = "";
+	Question[i].sentence[1] = "";
+
+	Question[i].Image_Graphic = LoadGraph("");//問題のイメージ画像を変数に読み込む
+
+	//選択肢
+	Question[i].choices[0] = "A：";
+	Question[i].choices[1] = "B：";
+	Question[i].choices[2] = "C：";
+	Question[i].choices[3] = "D：";
+
+	//正解（A、B、C、Dのどれかを入力）
+	Question[i].answer = 'C';
+
+	//問題の解説
+	Question[i].tips[0] = "";
+	Question[i].tips[1] = "千";
+}
+//8問目
+if (Question[i].num == 7)
 {
 	//問題文
 	Question[i].sentence[0] = "次のうち、千葉県での生産量が";
@@ -32,7 +54,7 @@ if (Question[i].num == 1)
 	//選択肢
 	Question[i].choices[0] = "A：Apple社";
 	Question[i].choices[1] = "B：落花生";
-	Question[i].choices[2] = "C：メロン";
+	Question[i].choices[2] = "C：ふなっしー";
 	Question[i].choices[3] = "D：すいか";
 
 	//正解（A、B、C、Dのどれかを入力）
